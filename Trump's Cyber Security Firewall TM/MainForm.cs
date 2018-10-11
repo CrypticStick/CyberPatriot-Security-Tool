61970500ab6f3d49656bbe7b34d52e2d9ca5fb35
Jakob Stickles <theawsomejs@outlook.com>
2018-10-11 16:49:39 -0700

    Code fully working!! - Textbox no longer crashes due to invalid input - Users are set to admin and normal user automatically - Unauthorized users are automatically removed - Fancy IntelliSense summaries of custom methods added - Many debug log messages and flashing command prompts (for added "WOW!" factor :P)

--- Trump's Cyber Security Firewall TM/MainForm.cs
+++ Trump's Cyber Security Firewall TM/MainForm.cs
@@ -1,16 +1,11 @@
 ﻿using Microsoft.Win32;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Security;
-using System.Security.AccessControl;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Collections;
+using System.Diagnostics;
 
 namespace Trump_s_Cyber_Security_Firewall_TM
 {
@@ -22,12 +17,18 @@ namespace Trump_s_Cyber_Security_Firewall_TM
             TxtBoxInfo.AppendText(Environment.UserName);
         }
 
+        /// <summary>
+        /// Logs the specified string to TxtBoxInfo on a new line.
+        /// </summary>
         private void Log(string message)
         {
             TxtBoxInfo.AppendText(Environment.NewLine + message);
 
         }
 
+        /// <summary>
+        /// Converts an array of bytes into a hexideciamal string.
+        /// </summary>
         public static string ByteArrayToString(byte[] ba)
         {
             StringBuilder hex = new StringBuilder(ba.Length * 2);
@@ -36,12 +37,29 @@ namespace Trump_s_Cyber_Security_Firewall_TM
             return hex.ToString();
         }
 
+        /// <summary>
+        /// Edits a byte array at a specified index.
+        /// <para>Requires an unsigned 8-bit number, but sets the following byte to zero.</para>
+        /// <para>This method is designed specifically for editing the Windows registry for auditing.</para>
+        /// </summary>
+        /// <param name="array">A reference to the byte array being edited.</param>
+        /// <param name="index">The byte being edited.</param>
+        /// <param name="value">The new value for the specified byte.</param>
         public static void EditByteArray(ref byte[] array, int index, byte value)
         {
             array[index] = value;
             array[++index] = 0x00;
         }
 
+        /// <summary>
+        /// Edits a byte array at a specified index.
+        /// <para>Requires an unsigned 8-bit number, but sets the following byte to zero.</para>
+        /// <para>This method is designed specifically for editing the Windows registry for auditing.</para>
+        /// </summary>
+        /// <param name="array">A reference to the byte array being edited.</param>
+        /// <param name="start">The index of the first byte to be edited.</param>
+        /// <param name="end">The index of the final byte to be edited.</param>
+        /// <param name="value">The new value for the specified bytes.</param>
         public static void EditByteArray(ref byte[] array, int start, int end, byte value)
         {
             int index = start;
@@ -51,13 +69,19 @@ namespace Trump_s_Cyber_Security_Firewall_TM
             }
         }
 
-        private RegistryKey AccessRegistryKey(string key, bool editable)
+        /// <summary>
+        /// Provides a <see cref="Microsoft.Win32.RegistryKey"/> object for the specified registry directory.
+        /// </summary>
+        /// <param name="key">The registry key being accessed.</param>
+        /// <param name="writable">Whether the key can be written to.</param>
+        /// <returns>Returns a <see cref="Microsoft.Win32.RegistryKey"/> object, or null if an error occurs.</returns>
+        private RegistryKey AccessRegistryKey(string key, bool writable)
         {
             RegistryKey regKey = Registry.LocalMachine;
             RegistryKey OGKey = regKey;
             try
             {
-                regKey = Registry.LocalMachine.OpenSubKey(key, editable);
+                regKey = Registry.LocalMachine.OpenSubKey(key, writable);
             }
             catch (SecurityException ex)
             {
@@ -76,6 +100,136 @@ namespace Trump_s_Cyber_Security_Firewall_TM
                 return null;
             }
         }
+
+        /// <summary>
+        /// Removes the specified user from the system.
+        /// </summary>
+        /// <param name="username">The user to be deleted.</param>
+        /// <param name="retainFiles">Whether the user's files should be saved.</param>
+        private void RemoveUser(string username, bool retainFiles)
+        {
+            Log($"Trying to delete {username}...");
+
+            string theoreticalDirectory = $@"C:\Users\{username}";
+
+            RegistryKey userIDs = AccessRegistryKey(
+            @"SECURITY\SAM\Domains\Account\Users",
+            true
+            );
+
+            if (userIDs != null)
+            {
+                if(!userIDs.OpenSubKey("Names").GetSubKeyNames().Contains(username))
+                {
+                    Log("User does not exist. Continuing...");
+                    return;
+                }
+
+                RegistryKey key = AccessRegistryKey(
+                @"SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList",
+                true
+                );
+
+                RegistryKey subKey;
+                if (key != null)
+                {
+                    Log("Successfully accessed ProfileList.");
+                    String[] profiles = key.GetSubKeyNames();
+                    String currentPath;
+                    bool found = false;
+                    foreach (string chaoticString in profiles)
+                    {
+                        subKey = key.OpenSubKey(chaoticString);
+                        currentPath = subKey.GetValue("ProfileImagePath").ToString();
+                        if (subKey.GetValue("ProfileImagePath").ToString()
+                            .Equals(theoreticalDirectory))
+                        {
+                            found = true;
+                            Log("Found user! Attempting to remove user files...");
+                            try
+                            {
+                                key.DeleteSubKey(chaoticString);
+                                Log($"Removed {username}'s profile.");
+                                if (retainFiles)
+                                {
+                                    System.IO.Directory.Move(theoreticalDirectory, theoreticalDirectory + " (Deleted)");
+                                    Log($"{username}'s file directory successfully renamed.");
+                                }
+                                else
+                                {
+                                    System.IO.Directory.Delete(theoreticalDirectory);
+                                    Log($"{username}'s files successfully deleted.");
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                Log("Warning: Could not remove user files!!");
+                            }
+                        }
+                    }
+                    if (!found) Log("Didn't find user in ProfileList. Continuing...");
+                    key.Close();
+                }
+                else
+                {
+                    Log("ERROR: ProfileList key could not be opened. Do you have permission?");
+                    key.Close();
+                    return;
+                }
+
+                Log($"Attempting to export profile info of {username}...");
+                String exportKeyCmd = $"/C regedit /E \"C:\\Windows\\Temp\\{username}_type.reg\" \"HKEY_LOCAL_MACHINE\\SECURITY\\SAM\\Domains\\Account\\Users\\Names\\{username}\"";
+
+                Process exportUser = Process.Start("CMD.exe", exportKeyCmd);
+                if (!exportUser.WaitForExit(5000))
+                {
+                    Log("User export timed out. Reading will likely fail.");
+                }
+                string regContents;
+                try
+                {
+                    regContents = System.IO.File.ReadAllText($@"C:\Windows\Temp\{username}_type.reg");
+                    System.IO.File.Delete($@"C:\Windows\Temp\{username}_type.reg");
+                }
+                catch (Exception ex)
+                {
+                    Log(ex.Message);
+                    Log("ERROR: Cannot delete user - failed to read user info.");
+                    return;
+                }
+                int startIndex = regContents.IndexOf('(') + 1;
+                int substringLength = regContents.IndexOf(')') - startIndex;
+                string regType = regContents.Substring(startIndex, substringLength);
+
+                Log("Successfully read user info! Attempting to fully wipe user...");
+
+                foreach (string id in userIDs.GetSubKeyNames())
+                {
+                    if (id.Contains(regType.ToUpper()))
+                    {
+                        try
+                        {
+                            userIDs.DeleteSubKey(id);
+                            userIDs.OpenSubKey("Names", true).DeleteSubKey(username);
+                            Log($"User {username} has been fully removed!");
+                            userIDs.Close();
+                            return;
+                        } catch (Exception ex)
+                        {
+                            Log(ex.Message);
+                        }
+                    }
+                }
+                Log($"User {username} not found.");
+            }
+            else
+            {
+                Log("Failed to access users.");
+            }
+            Log($"ERROR: {username} failed to delete.");
+            userIDs.Close();
+        }
+
 
         private void BtnAuditing_Click(object sender, EventArgs e)
         {
@@ -98,105 +252,49 @@ namespace Trump_s_Cyber_Security_Firewall_TM
             }
         }
 
-        private void RemoveUser(string username)
-        {
-            Log($"Trying to delete {username}...");
-
-            string theoreticalDirectory = $@"C:\Users\{username}";
-
-            RegistryKey key = AccessRegistryKey(
-                @"SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList",
-                true
-                );
-
-            RegistryKey subKey;
-            if (key != null)
-            {
-                Log("Successfully accessed ProfileList.");
-                String[] profiles = key.GetSubKeyNames();
-                String currentPath;
-                foreach (string chaoticString in profiles)
-                {
-                    subKey = key.OpenSubKey(chaoticString);
-                    currentPath = subKey.GetValue("ProfileImagePath").ToString();
-                    Log($"Found '{currentPath}'");
-                    if (subKey.GetValue("ProfileImagePath").ToString()
-                        .Equals(theoreticalDirectory))
-                    {
-                        Log("Found user! Deleting...");
-                        try
-                        {
-                            key.DeleteSubKey(chaoticString);
-                            System.IO.Directory.Move(theoreticalDirectory, theoreticalDirectory + " (Deleted)");
-                        }
-                        catch (Exception ex)
-                        {
-                            Log("Account deletion failed!");
-                            return;
-                        }
-                        key.Close();
-                        return;
-                    }
-                }
-
-                String exportKeyCmd = $@"/C regedit /E 'C:\Windows\Temp\{username}_type.reg' 'HKEY_LOCAL_MACHINE\SECURITY\SAM\Domains\Account\Users\Names\{username}'";
-
-                System.Diagnostics.Process.Start("CMD.exe", exportKeyCmd);
-                string regContents;
-                try
-                {
-                    regContents = System.IO.File.ReadAllText($@"C:\Windows\Temp\{username}_type.reg");
-                    System.IO.File.Delete($@"C:\Windows\Temp\{username}_type.reg");
-                }
-                catch (Exception ex)
-                {
-                    Log(ex.Message);
-                    Log("Cannot fully delete user: failed to read user info.");
-                    return;
-                }
-                int startIndex = regContents.IndexOf('(') + 1;
-                int substringLength = regContents.IndexOf(')') - startIndex;
-                string regType = regContents.Substring(startIndex, substringLength);
-
-                Log(regType);
-                Log("Successfully read type!");
-            }
-            Log("Profile for specified user not found.");
-        }
-
         private void BtnUsers_Click(object sender, EventArgs e)
         {
-            ArrayList userList = new ArrayList();
-            userList.AddRange(TxtBoxUsers.Text.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries));
-
-            for (int i = 0; i < userList.Count; i++) //removes everything except users
+            ArrayList userList;
+            ArrayList admins;
+            try
             {
-                string line = userList[i].ToString().Trim();
-
-                if (line.Contains("password:") || line.Equals(""))
+                userList = new ArrayList();
+                userList.AddRange(TxtBoxUsers.Text.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries));
+                if (userList.Count < 2 || !userList.Contains("Authorized Administrators:") || !userList.Contains("Authorized Users:"))
                 {
-                    userList.RemoveAt(i);
-                    continue;
+                    Log("Please paste \"Authorized Users\" and \"Authorized Administrators\" text in textbox.");
+                    return;
                 }
 
-                userList[i] = line;
+                for (int i = 0; i < userList.Count; i++) //removes everything except users
+                {
+                    string line = userList[i].ToString().Trim();
+
+                    if (line.Contains("password:") || line.Equals(""))
+                    {
+                        userList.RemoveAt(i);
+                        continue;
+                    }
+
+                    userList[i] = line;
+                }
+
+                userList.RemoveAt(0);
+                userList[0] = userList[0].ToString().
+                    Substring(0, userList[0].ToString().LastIndexOf(' '));
+
+                admins = new ArrayList(userList);
+
+                    admins.RemoveRange(
+                        admins.IndexOf("Authorized Users:"),
+                        admins.Count - admins.IndexOf("Authorized Users:")
+                        );
             }
-
-            userList.RemoveAt(0);
-            userList[0] = userList[0].ToString().Split(' ')[0];
-
-            ArrayList admins = new ArrayList(userList);
-            try
-            { 
-                admins.RemoveRange(
-                    admins.IndexOf("Authorized Users:"), 
-                    admins.Count - admins.IndexOf("Authorized Users:")
-                    );
-            } catch (Exception ex) {             
+            catch (Exception ex)
+            {
                 Log("This list of names doesn't work.");
                 return;
             }
-
             RegistryKey usersKey = AccessRegistryKey(@"SECURITY\SAM\Domains\Account\Users\Names", false);
 
             if (usersKey != null)
@@ -215,7 +313,7 @@ namespace Trump_s_Cyber_Security_Firewall_TM
 
                     if (!userList.Contains(user))
                     {
-                        RemoveUser(user);
+                        RemoveUser(user,true);
                         continue;
                     }
 
@@ -229,7 +327,7 @@ namespace Trump_s_Cyber_Security_Firewall_TM
                         Log($"Setting {user} as normal user...");
                         userAdminCmd = $"/C net localgroup \"Administrators\" \"{user}\" /DELETE";
                     }
-                    System.Diagnostics.Process.Start("CMD.exe", userAdminCmd);
+                    Process.Start("CMD.exe", userAdminCmd);
                 }
 
             }
@@ -240,5 +338,11 @@ namespace Trump_s_Cyber_Security_Firewall_TM
 
         }
 
+        //TODO: Add new button - Build the Wall! (Sets up security)
+        /*
+         * 'Build the Wall!' should be able to configure windows security settings 
+         * (UAC, Firewall, Password Requirements, etc.)
+         * and install antimalaware (Malawarebytes) automatically.
+         */
     }
 }
