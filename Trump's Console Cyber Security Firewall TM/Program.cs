b4334d28ca138164ed68fb9a7d77c4f346993912
Jakob Stickles <theawsomejs@outlook.com>
2018-11-14 17:33:00 -0800

    How does Github work Also new Linux project perhaps

---
+++ Trump's Console Cyber Security Firewall TM/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Trump_s_Console_Cyber_Security_Firewall_TM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
