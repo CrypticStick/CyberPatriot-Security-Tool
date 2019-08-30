848eec11c6c033127166d95c6f40c26d0770a738
Jakob Stickles <jnlstickles@yahoo.com>
2019-08-29 18:06:00 -0700

    Added menu tabs, Colorful.Console library, and WIP buttons

--- Trump's Console Cyber Security Firewall TM/Program.cs
+++ Trump's Console Cyber Security Firewall TM/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Diagnostics;
 using static Trump_s_Console_Cyber_Security_Firewall_TM.Label;
 using static Trump_s_Console_Cyber_Security_Firewall_TM.MenuItem;
@@ -9,20 +10,21 @@ namespace Trump_s_Console_Cyber_Security_Firewall_TM
     class Program
     {
         static private Screen MyScreen;
-        static private Menu MainMenu;
-        static private Menu ConfigMenu;
+        static private Menu MainMenu, ConfigMenu;
         private static readonly ConsoleColor StartColor = Console.BackgroundColor;
 
         static void Main(string[] args)
         {
-            MainMenu = new Menu("Main", ConsoleColor.DarkRed);
-            MainMenu.Add(new Label("Bazinga", AnchorSide.Left | AnchorSide.Top, 10, 4));
-            //MainMenu.Add(new Button());
+            MainMenu = new Menu("Main", Color.DarkRed);
+            MainMenu.Add(new Label("Bazinga", AnchorSide.Left | AnchorSide.Top, 10, 4, Color.Black));
+            MainMenu.Add(new Button("Button 1", AnchorSide.Right | AnchorSide.Top, 20, 10, Color.BlueViolet));
+            MainMenu.Add(new Button("Button 2", AnchorSide.Right | AnchorSide.Bottom, 20, 5));
 
-            ConfigMenu = new Menu("Config",ConsoleColor.DarkGreen);
+            ConfigMenu = new Menu("Config",Color.DarkGreen);
             ConfigMenu.Add(new Label("Kachow", AnchorSide.Right | AnchorSide.Bottom, 10, 4));
 
             MyScreen = new Screen(MainMenu);
+            MyScreen.AddMenu(ConfigMenu);
 
             MyScreen.WindowResizedEvent += OnWindowResized;
             MyScreen.KeyReceivedEvent += OnKeyReceived;
@@ -41,21 +43,35 @@ namespace Trump_s_Console_Cyber_Security_Firewall_TM
                 case (ConsoleKey.Q):
                     Quit();
                     break;
-                case (ConsoleKey.M):
-                    MyScreen.SetMenu(MainMenu);
-                    break;
-                case (ConsoleKey.C):
-                    MyScreen.SetMenu(ConfigMenu);
-                    break;
-                default:
-                    MyScreen.Reload();
-                    break;
             }
+        }
+
+        static string ReplaceStringInFile(string filepath, string toReplace, string replacement)
+        {
+            return $"sed -i '.bak' -e 's/{toReplace}/{replacement}/g' {filepath}".Bash();
         }
 
         static void Secure()
         {
-            var output = "echo \"this is a test\" > testFileha.txt".Bash();
+            string file = "/etc/ssh/sshd_config";
+            ReplaceStringInFile(file, "Port 22", "Port 2020");
+            ReplaceStringInFile(file, "Protocol 1", "Protocol 2");
+            ReplaceStringInFile(file, "PermitRootLogin yes", "PermitRootLogin no");
+            ReplaceStringInFile(file, "ChallengeResponseAuthentication yes", "ChallengeResponseAuthentication no");
+            ReplaceStringInFile(file, "PasswordAuthentication yes", "PasswordAuthentication no");
+            ReplaceStringInFile(file, "UsePAM yes", "UsePAM no");
+            ReplaceStringInFile(file, "PubkeyAuthentication yes", "PubkeyAuthentication no");
+            ReplaceStringInFile(file, "PermitEmptyPasswords yes", "PermitEmptyPasswords no");
+
+            "echo \"install usb-storage /bin/true\" > /etc/modprobe.d/no-usb".Bash();
+            "sudo ufw deny 22".Bash();
+            "sudo ufw deny 2020/tcp".Bash();
+            "sudo apt-get update && sudo apt-get upgrade".Bash();
+
+            foreach (Label mi in MyScreen.GetMenu().GetMenuItems())
+            {
+                mi.EditText("WE DID IT");
+            }
         }
 
         static void Quit()
--- Trump's Console Cyber Security Firewall TM/Screen.cs
+++ Trump's Console Cyber Security Firewall TM/Screen.cs
@@ -2,43 +2,91 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
+using System.Drawing;
+using Console = Colorful.Console;
 
 namespace Trump_s_Console_Cyber_Security_Firewall_TM
 {
     class Screen
-    { 
+    {
+        List<Menu> Menus = new List<Menu>();
         Menu CurrentMenu;
         public event EventHandler WindowResizedEvent;
         public event EventHandler<KeyReceivedArgs> KeyReceivedEvent;
-        int LastWidth = 0;
-        int LastHeight = 0;
-
-        Thread t1;
-        Thread t2;
+        int LastWidth = 0, LastHeight = 0;
+        Thread t1, t2, t3;
 
         public Screen(Menu defaultMenu)
         {
             SetMenu(defaultMenu);
 
             WindowResizedEvent += OnWindowResized;
+            KeyReceivedEvent += OnKeyReceived;
 
             t1 = new Thread(() => CheckWindow());
             t1.Start();
 
             t2 = new Thread(() => CheckInput());
             t2.Start();
+
+            t3 = new Thread(() => CheckAnimation());
+            t3.Start();
         }
 
         public void SetMenu(Menu newMenu)
         {
+            if (!Menus.Contains(newMenu))
+                Menus.Add(newMenu);
             CurrentMenu = newMenu;
             Reload();
         }
 
+        public void AddMenu(Menu newMenu)
+        {
+            if (!Menus.Contains(newMenu))
+            {
+                Menus.Add(newMenu);
+                Reload();
+            }
+        }
+
+        public Menu GetMenu()
+        {
+            return CurrentMenu;
+        }
+
+        public List<Menu> GetMenus()
+        {
+            return Menus;
+        }
+
         public void Reload()
         {
+            Console.BackgroundColor = CurrentMenu.GetBackground();
+            Console.Clear();
+            DrawTabs();
             CurrentMenu.WriteMenu();
+        }
+
+        void DrawTabs()
+        {
+            int totalChar = Console.WindowWidth;
+            StringBuilder st = new StringBuilder();
+            int totalchar = 0;
+            foreach (Menu menu in Menus)
+            {
+                st = new StringBuilder();
+                Console.BackgroundColor = menu.GetBackground();
+                st.Append(' ', (totalChar/Menus.Count - menu.GetTitle().Length) / 2);
+                st.Append("{0}"+menu.GetTitle().Substring(1));
+                st.Append(' ', (totalChar/Menus.Count - menu.GetTitle().Length) / 2);
+                Console.WriteFormatted(st.ToString(), Color.Black, Color.White, menu.GetTitle().ToCharArray()[0]);
+                totalchar += st.ToString().Length-2;
+            }
+            st = new StringBuilder();
+            st.Append(' ',Console.WindowWidth - totalchar);
+            Console.WriteLine(st.ToString());
+            Console.BackgroundColor = CurrentMenu.GetBackground();
         }
 
         void CheckWindow()
@@ -64,18 +112,131 @@ namespace Trump_s_Console_Cyber_Security_Firewall_TM
             }
         }
 
+        void CheckAnimation()
+        {
+            MenuItem item;
+            bool state = false;
+            while (true)
+            {
+                Thread.Sleep(500);
+                Console.CursorVisible = false;
+                item = CurrentMenu.GetHighlightedItem();
+                if (item != null)
+                {
+                    if (state) {
+                        Console.BackgroundColor = Color.White;
+                        item.WriteItem();
+                        Console.BackgroundColor = CurrentMenu.GetBackground();
+                    } else {
+                        Console.BackgroundColor = CurrentMenu.GetBackground();
+                        item.WriteItem();
+                    }
+                    state = !state;
+                }
+                Console.SetCursorPosition(0, Console.WindowHeight - 1);
+                Console.CursorVisible = true;
+            }
+        }
+
         void OnWindowResized(object sender, EventArgs e)
         {
             Reload();
+        }
+
+        void OnKeyReceived(object sender, KeyReceivedArgs e)
+        {
+            MenuItem closestButton = null;
+            switch (e.KeyInfo.Key)
+            {
+                case (ConsoleKey.UpArrow):
+                    if (CurrentMenu.GetHighlightedItem() == null) return;
+                    foreach (MenuItem item in CurrentMenu.GetMenuItems())
+                        if (item.GetType().Equals(typeof(Button)))
+                        {
+                            if (CurrentMenu.GetHighlightedItem().Equals(item)) break;
+                            if (item.GetVertPos() < CurrentMenu.GetHighlightedItem().GetVertPos())
+                            {
+                                if (closestButton == null)
+                                    closestButton = item;
+                                else if (CurrentMenu.GetHighlightedItem().GetVertPos() - closestButton.GetVertPos() >
+                                    CurrentMenu.GetHighlightedItem().GetVertPos() - item.GetVertPos())
+                                    closestButton = item;
+                            }
+                        }
+                    break;
+                case (ConsoleKey.LeftArrow):
+                    if (CurrentMenu.GetHighlightedItem() == null) return;
+                    foreach (MenuItem item in CurrentMenu.GetMenuItems())
+                        if (item.GetType().Equals(typeof(Button)))
+                        {
+                            if (CurrentMenu.GetHighlightedItem().Equals(item)) break;
+                            if (item.GetHoriPos() < CurrentMenu.GetHighlightedItem().GetHoriPos())
+                            {
+                                if (closestButton == null)
+                                    closestButton = item;
+                                else if (CurrentMenu.GetHighlightedItem().GetHoriPos() - closestButton.GetHoriPos() >
+                                    CurrentMenu.GetHighlightedItem().GetHoriPos() - item.GetHoriPos())
+                                    closestButton = item;
+                            }
+                        }
+                    break;
+                case (ConsoleKey.DownArrow):
+                    if (CurrentMenu.GetHighlightedItem() == null) return;
+                    foreach (MenuItem item in CurrentMenu.GetMenuItems())
+                        if (item.GetType().Equals(typeof(Button)))
+                        {
+                            if (CurrentMenu.GetHighlightedItem().Equals(item)) break;
+                            if (item.GetVertPos() > CurrentMenu.GetHighlightedItem().GetVertPos())
+                            {
+                                if (closestButton == null)
+                                    closestButton = item;
+                                else if (CurrentMenu.GetHighlightedItem().GetVertPos() - closestButton.GetVertPos() <
+                                    CurrentMenu.GetHighlightedItem().GetVertPos() - item.GetVertPos())
+                                    closestButton = item;
+                            }
+                        }
+                    break;
+                case (ConsoleKey.RightArrow):
+                    if (CurrentMenu.GetHighlightedItem() == null) return;
+                    foreach (MenuItem item in CurrentMenu.GetMenuItems())
+                        if (item.GetType().Equals(typeof(Button)))
+                        {
+                            if (CurrentMenu.GetHighlightedItem().Equals(item)) break;
+                            if (item.GetHoriPos() > CurrentMenu.GetHighlightedItem().GetHoriPos())
+                            {
+                                if (closestButton == null)
+                                    closestButton = item;
+                                else if (CurrentMenu.GetHighlightedItem().GetHoriPos() - closestButton.GetHoriPos() <
+                                    CurrentMenu.GetHighlightedItem().GetHoriPos() - item.GetHoriPos())
+                                    closestButton = item;
+                            }
+                        }
+                    break;
+                default:
+                    foreach (Menu menu in Menus)
+                    {
+                        if (e.KeyInfo.Key.ToString().ToLower().ToCharArray()[0].Equals(
+                            menu.GetTitle().ToLower().ToCharArray()[0]))
+                        {
+                            SetMenu(menu);
+                            break;
+                        }
+                    }
+                    Console.SetCursorPosition(0, Console.WindowHeight - 1);
+                    break;
+            }
+            if (closestButton != null)
+                CurrentMenu.SetHighlightedItem(closestButton);
         }
     }
 
     internal class Menu
     {
         List<MenuItem> Items = new List<MenuItem>();
+        MenuItem HighlightedItem = null;
 
         string Title = "New Menu";
-        ConsoleColor Background = ConsoleColor.DarkBlue;
+        Color Background = Color.DarkBlue;
 
         public Menu()
         {
@@ -86,7 +247,7 @@ namespace Trump_s_Console_Cyber_Security_Firewall_TM
             Title = title;
         }
 
-        public Menu(string title, ConsoleColor background)
+        public Menu(string title, Color background)
         {
             Title = title;
             Background = background;
@@ -98,10 +259,20 @@ namespace Trump_s_Console_Cyber_Security_Firewall_TM
             return this;
         }
 
-        public Menu SetBackground(ConsoleColor background)
+        public string GetTitle()
+        {
+            return Title;
+        }
+
+        public Menu SetBackground(Color background)
         {
             Background = background;
             return this;
+        }
+
+        public Color GetBackground()
+        {
+            return Background;
         }
 
         public Menu Add(MenuItem item)
@@ -110,14 +281,36 @@ namespace Trump_s_Console_Cyber_Security_Firewall_TM
             return this;
         }
 
+        public List<MenuItem> GetMenuItems()
+        {
+            return Items;
+        }
+
+        public MenuItem GetHighlightedItem()
+        {
+            return HighlightedItem;
+        }
+
+        public Menu SetHighlightedItem(MenuItem item)
+        {
+            HighlightedItem = item;
+            return this;
+        }
+
         public void WriteMenu()
         {
             Console.CursorVisible = false;
             Console.BackgroundColor = Background;
-            Console.Clear();
             Console.Title = $"Trump's Console Cyber Security Firewall TM - {Title}";
             Console.WriteLine(CreateHeader(Title));
             Console.WriteLine();
+
+            if (HighlightedItem == null)
+                foreach (MenuItem item in Items)
+                    if (item.GetType().Equals(typeof(Button))) {
+                        HighlightedItem = item;
+                        break;
+                    }
 
             foreach (MenuItem item in Items)
                 item.WriteItem();
@@ -130,9 +323,9 @@ namespace Trump_s_Console_Cyber_Security_Firewall_TM
         {
             int totalChar = Console.WindowWidth;
             StringBuilder st = new StringBuilder();
-            st.Append('>', (totalChar - title.Length) / 2);
-            st.Append(title);
-            st.Append('<', (totalChar - title.Length) / 2);
+            st.Append('>', (totalChar - title.Length) / 2 - 1);
+            st.Append(" " + title + " ");
+            st.Append('<', (totalChar - title.Length) / 2 - 1);
             return st.ToString();
         }
     }
@@ -140,6 +333,7 @@ namespace Trump_s_Console_Cyber_Security_Firewall_TM
     internal abstract class MenuItem
     {
         protected AnchorSide Anchor = AnchorSide.Left | AnchorSide.Top;
+        protected Color color = Color.White;
         protected int DistVert = 0;
         protected int DistHori = 0;
 
@@ -157,6 +351,16 @@ namespace Trump_s_Console_Cyber_Security_Firewall_TM
             return this;
         }
 
+        public int GetHoriPos()
+        {
+            return DistHori;
+        }
+
+        public int GetVertPos()
+        {
+            return DistVert;
+        }
+
         public enum AnchorSide
         {
             Left = 1,
@@ -169,6 +373,17 @@ namespace Trump_s_Console_Cyber_Security_Firewall_TM
         {
             Anchor = anchor;
             return this;
+        }
+
+        public MenuItem SetColor(Color color)
+        {
+            this.color = color;
+            return this;
+        }
+
+        public Color GetColor()
+        {
+            return color;
         }
 
         public void MoveCursorToWrite()
@@ -191,7 +406,7 @@ namespace Trump_s_Console_Cyber_Security_Firewall_TM
     {
         public event EventHandler ButtonClickedEvent;
 
-        string Text = "New Label";
+        string Text = "New Button";
 
         public Button()
         {
@@ -216,6 +431,15 @@ namespace Trump_s_Console_Cyber_Security_Firewall_TM
             DistHori = distHori;
         }
 
+        public Button(string text, AnchorSide anchor, int distHori, int distVert, Color color)
+        {
+            Text = text;
+            Anchor = anchor;
+            DistVert = distVert;
+            DistHori = distHori;
+            this.color = color;
+        }
+
         public Button EditText(string text)
         {
             Text = text;
@@ -225,7 +449,7 @@ namespace Trump_s_Console_Cyber_Security_Firewall_TM
         public override void WriteItem()
         {
             MoveCursorToWrite();
-            Console.Write(Text);
+            Console.Write(Text,color);
         }
     }
 
@@ -257,6 +481,15 @@ namespace Trump_s_Console_Cyber_Security_Firewall_TM
 
         }
 
+        public Label(string text, AnchorSide anchor, int distHori, int distVert, Color color)
+        {
+            Text = text;
+            Anchor = anchor;
+            DistVert = distVert;
+            DistHori = distHori;
+            this.color = color;
+        }
+
         public Label EditText(string text)
         {
             Text = text;
@@ -266,7 +499,7 @@ namespace Trump_s_Console_Cyber_Security_Firewall_TM
         public override void WriteItem()
         {
             MoveCursorToWrite();
-            Console.Write(Text);
+            Console.Write(Text, color);
         }
     }
 
